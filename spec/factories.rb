70338081d47b449174240286bc7e2e3e4b84feb0
Jeremy Gilman <jeremy.gilman@technekes.com>
2011-06-10 14:56:36 -0400

    Done with users edit, index, and destroy actions'

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,10 @@
-# By using the symbol ':user', we get Factory Girl to simulate the User model.
 Factory.define :user do |user|
-  user.name                  "Dodo Bird"
-  user.email                 "Dodo.Bird@birdscanfly.com"
-  user.password              "extinction"
-  user.password_confirmation "extinction"
+  user.name                  "Dodo"
+  user.email                 "Dodo@birds.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
+
+Factory.sequence :email do |n|
+  "person-#{n}@example.com"
 end
