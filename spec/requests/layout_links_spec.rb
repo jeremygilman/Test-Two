95f384357af070fe177e1818f306ef3b71d9ee2c
Jeremy Gilman <jeremy.gilman@technekes.com>
2011-06-10 10:01:40 -0400

    Added tests for user sign-up fields

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -25,7 +25,7 @@ describe "LayoutLinks" do
   it "should have a signup page at '/signup'" do
       get '/signup'
       response.should have_selector('title', :content => "Sign up")
-    end
+  end
     
   it "should have the right links on the layout" do
     visit root_path
